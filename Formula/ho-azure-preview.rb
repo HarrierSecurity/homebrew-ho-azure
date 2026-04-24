6dadd5c821f8f130f9be120ed97e817ce8c654d5
TacoRocket <farleycolby@gmail.com>
2026-04-24 15:51:56 -0500

    brew: update ho-azure-preview to v1.2.0 (#6)
    Co-authored-by: TacoRocket <9084308+TacoRocket@users.noreply.github.com>

--- Formula/ho-azure-preview.rb
+++ Formula/ho-azure-preview.rb
@@ -1,8 +1,8 @@
 class HoAzurePreview < Formula
   desc "Preview Homebrew formula for HarrierOps Azure"
   homepage "https://github.com/HarrierSecurity/HarrierOps-Azure"
-  url "https://github.com/HarrierSecurity/HarrierOps-Azure/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "1ead205b533750a36b7fbba653df872ce257587af63258a39a97294c62ccbd82"
+  url "https://github.com/HarrierSecurity/HarrierOps-Azure/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "0163e98d9e906d5833b72db83a0dc7369d1c2ef9330051b45bdaa5d2f91107b9"
   license "MIT"
   head "https://github.com/HarrierSecurity/HarrierOps-Azure.git", branch: "main"
 
